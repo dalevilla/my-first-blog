ec6c677f4a59ac189b372a901ef94ee583461eaa
Arvin Dale Villa <arvilla3483@gmail.com>
2023-06-28 09:55:38 +0800

    Add templates to dipslay posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post #.models in this case is same dir or current dir
+#not hidden
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #created vs published not the same, I may have not  added published
+    #date to Blog 3
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
